3ec308bbf2fd6279d1064dc4173dde6f9717bf50
Joao-Gontijo <jvgontijo166@gmail.com>
2022-02-25 16:52:51 -0300

    data types

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -1,3 +1,26 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  int idade = 20;
+  int valorHexadecimal = 0x00001A;
+
+  final double altura;
+  altura = 1.86;
+
+  bool geek = true;
+
+  const String nome = 'João Victor';
+  String sobrenome = "Gontijo";
+  // concatenação
+  // print('meu nome completo é ' + nome + ' ' + sobrenome);
+
+  //frase utilizando Referenciação - $variável
+  String frase = 'Meu nome é $nome \n'
+      'e tenho $idade anos de idade';
+  print(frase);
+
+  List<String> listaVazia = [];
+  List<String> listaNomes = ['João', 'Ana', 'Aldo', 'Marcos', 'Gabriel'];
+
+  List<dynamic> joao = [idade, altura, nome];
+  String apresentacao = 'Meu nome é ${joao[2]} e tenho ${joao[1]} anos';
+  print(apresentacao);
 }
